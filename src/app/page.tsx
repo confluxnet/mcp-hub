94efc7bb61db75ed2619b5a62181c289ef128d7c
Nahee-Park <nahee@ewha.ac.kr>
2025-04-13 10:36:27 +0900

    feat: fix rendering error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
     handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
-  }, [loadMcps]);
+  }, []);
 
   // Find MCPs based on use case
   const findMcpsForUseCase = async () => {
